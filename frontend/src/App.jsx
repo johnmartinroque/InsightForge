c23475420b1b560fafa30e8d77e63c3f7f94e85f
johnmartinroque <johnmartinroque@gmail.com>
2026-07-11 21:21:46 +0800

    react route dom and navigation setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import Home from "./screens/Home";
 function App() {
-  return <div className="text-center">App</div>;
+  return (
+    <div className="text-center">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
