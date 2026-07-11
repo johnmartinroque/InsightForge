3199b7241b8d9a5d3e44939d9302e3c6553ddc93
johnmartinroque <johnmartinroque@gmail.com>
2026-07-11 21:26:37 +0800

    login screen with login form component

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LoginForm from "../components/LoginForm";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
 }
 
 export default Login;
