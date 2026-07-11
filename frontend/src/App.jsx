c395c7f58e3851da0c6f758f46b12f40b35ccf7b
johnmartinroque <johnmartinroque@gmail.com>
2026-07-11 21:36:18 +0800

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import Home from "./screens/Home";
+import Header from "./components/Header";
 function App() {
   return (
     <div className="text-center">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
