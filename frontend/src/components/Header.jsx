b622d8ecf09af00a55918eb824b2035348f99bd4
johnmartinroque <johnmartinroque@gmail.com>
2026-07-15 21:56:33 +0800

    remove commented-out logo and mobile menu code from Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -80,11 +80,13 @@ function Header() {
         <nav className="bg-gray-50 border-b border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 dark:border-gray-700 transition-colors duration-200">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Link to="/" className="flex items-center">
+              {/*
               <img
                 src="https://flowbite.com/docs/images/logo.svg"
                 className="mr-3 h-6 sm:h-9"
                 alt="Flowbite Logo"
               />
+             */}
               <span className="self-center text-xl font-semibold whitespace-nowrap text-gray-900 dark:text-white">
                 InsightForge
               </span>
@@ -195,6 +197,7 @@ function Header() {
                 </svg>
               </button>
             </div>
+            {/* 
             <div
               className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
               id="mobile-menu-2"
@@ -251,6 +254,8 @@ function Header() {
                 </li>
               </ul>
             </div>
+            
+            */}
           </div>
         </nav>
       </header>
