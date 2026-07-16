8257339afb34aad392fe55c8d18e57b22b099377
johnmartinroque <johnmartinroque@gmail.com>
2026-07-16 13:27:52 +0800

    landing page functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import Home from "./screens/Home";
