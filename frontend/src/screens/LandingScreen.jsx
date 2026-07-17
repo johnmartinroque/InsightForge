2272ff1867c873b77c6438333f1c5d0b91e263dc
johnmartinroque <johnmartinroque@gmail.com>
2026-07-17 17:48:59 +0800

    landing page structure

--- frontend/src/screens/LandingScreen.jsx
+++ frontend/src/screens/LandingScreen.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function LandingScreen() {
-  return <div>LandingScreen</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-x-2 gap-y-3">
+        <div className="bg-red-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-blue-500 rounded-lg shadow-xl min-h-[50px] col-span-3"></div>
+        <div className="bg-green-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-yellow-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-purple-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-pink-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-indigo-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-teal-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-orange-500 rounded-lg shadow-xl min-h-[50px]"></div>
+        <div className="bg-cyan-500 rounded-lg shadow-xl min-h-[50px]"></div>
+      </div>
+    </div>
+  );
 }
 
 export default LandingScreen;
