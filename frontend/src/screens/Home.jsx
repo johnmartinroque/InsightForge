72d7431144fed8c9a13eae6463be52b85020ed75
johnmartinroque <johnmartinroque@gmail.com>
2026-08-03 11:31:48 +0800

    removed pdf viewer

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import PDFViewer from "../components/PDFViewer";
 import Dashboard from "../components/dashboard/Dashboard";
 
 function Home() {
   return (
     <div>
-      {/*  <PDFViewer /> */}
-
       <Dashboard />
     </div>
   );
